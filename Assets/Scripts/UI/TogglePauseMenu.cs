5fd16282a5f0e6eac9f8b50f1950f47cbdfbda65
Azalea4u <dnguyen@student.neumont.edu>
2025-02-10 09:35:14 -0700

    Added UI SFX to PauseMenu

--- Assets/Scripts/UI/TogglePauseMenu.cs
+++ Assets/Scripts/UI/TogglePauseMenu.cs
@@ -6,6 +6,11 @@ public class TogglePauseMenu : MonoBehaviour
 
     [SerializeField] GameObject PauseMenu_UI;
 
+    [Header("Audio")]
+    [SerializeField] private AudioSource Pause_SFX;
+    [SerializeField] private AudioSource Resume_SFX;
+    [SerializeField] private AudioSource Exit_SFX;
+
     private void Start()
     {
         PauseMenu_UI.SetActive(false);
@@ -13,13 +18,24 @@ public class TogglePauseMenu : MonoBehaviour
 
     public void Open_PauseMenu()
     {
+        Pause_SFX.Play();
+
         PauseMenu_UI.SetActive(true);
         GameManager.instance.PauseGame(true);
     }
 
     public void Close_PauseMenu()
     {
+        Resume_SFX.Play();
+
         PauseMenu_UI.SetActive(false);
         GameManager.instance.PauseGame(false);
+    }
+
+    public void Load_MainMenu()
+    {
+        Exit_SFX.Play();
+
+        // Load Lobby Scene
     }
 }
