c6dd71776b91fbfec08cb1080dca675d6e938520
MarcusReyes619 <129239355+MarcusReyes619@users.noreply.github.com>
2025-02-13 09:35:25 -0700

    Score Event done

---
+++ Assets/Scripts/ScriptableObjects/ScoreData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Events;
+[CreateAssetMenu(fileName = "Scroe", menuName = "Scriptable Objects/Scroe")]
+public class ScroeData : ScriptableObject
+{
+    public UnityAction<int> score;
+
+    public void Add(int value)
+    {
+        score?.Invoke(value);
+    }
+
+    public void Subscribe(UnityAction<int> function)
+    {
+        score += function;
+    }
+
+
+    public void UnSubscribe(UnityAction<int> function)
+    {
+        score -= function;
+    }
+}
